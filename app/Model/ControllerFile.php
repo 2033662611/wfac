2a5c8823252cae5570bdce7bc370875808494d72
2033662611 <2033662611@qq.com>
2013-08-24 17:02:37 +0800

    控制层文件改进：生成的文件应具有和正常创建的文件同样的权限，以便可能的修改，另外，如果指定了文件名，则应该在生成的代码中指定所使用的模型

--- app/Model/ControllerFile.php
+++ app/Model/ControllerFile.php
@@ -12,18 +12,25 @@ class ControllerFile extends AppModel {
 				$this->data [$this->alias] ['file_name'] = $class_name . '.php';
 			} else {
 				$class_name = substr ( $this->data [$this->alias] ['file_name'], 0, - 4 );
+				
+				$model_name = Inflector::classify ( $this->data [$this->alias] ['table_name'] );
+				$uses = "public \$uses = array (
+			'$model_name' 
+	);
+	";
 			}
 			
 			$contents = "<?php
 App::uses ( 'AppController', 'Controller' );
 class $class_name extends AppController {
-	public \$scaffold;
+	{$uses}public \$scaffold;
 }
 			";
 			$filename = ROOT . '/app/Controller/' . $this->data [$this->alias] ['file_name'];
 			
 			if (! file_exists ( $filename )) {
 				file_put_contents ( $filename, $contents );
+				chmod ( $filename, 0666 );
 			}
 		}
 		
