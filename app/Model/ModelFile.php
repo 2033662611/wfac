e12409f958541f69885a48e58701dec45228f2d6
2033662611 <2033662611@qq.com>
2013-08-24 17:56:50 +0800

    模型层文件使用方法：点击添加，输入表名后提交（不输入文件名时会自动生成标准的文件名），如果该模型层文件不存在，则会生成

--- app/Model/ModelFile.php
+++ app/Model/ModelFile.php
@@ -5,4 +5,35 @@ class ModelFile extends AppModel {
 	public $validate = array (
 			'table_name' => 'notempty' 
 	);
+	public function beforeSave($options = array()) {
+		if (empty ( $this->data [$this->alias] ['id'] )) {
+			if (empty ( $this->data [$this->alias] ['file_name'] )) {
+				$class_name = Inflector::classify ( $this->data [$this->alias] ['table_name'] );
+				$this->data [$this->alias] ['file_name'] = $class_name . '.php';
+			} else {
+				$class_name = substr ( $this->data [$this->alias] ['file_name'], 0, - 4 );
+			}
+			
+			if (empty ( $this->data [$this->alias] ['display_field'] )) {
+				$displayField = 'id';
+			} else {
+				$displayField = $this->data [$this->alias] ['display_field'];
+			}
+			
+			$contents = "<?php
+App::uses ( 'AppModel', 'Model' );
+class $class_name extends AppModel {
+	public \$displayField = '$displayField';
+}
+			";
+			$filename = ROOT . '/app/Model/' . $this->data [$this->alias] ['file_name'];
+			
+			if (! file_exists ( $filename )) {
+				file_put_contents ( $filename, $contents );
+				chmod ( $filename, 0666 );
+			}
+		}
+		
+		return true;
+	}
 }
