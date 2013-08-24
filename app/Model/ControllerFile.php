770bceb6ee816b003beb704076174f3e652bad88
2033662611 <2033662611@qq.com>
2013-08-24 15:39:23 +0800

    控制层文件使用方法：点击添加，输入表名后提交（不输入文件名时会自动生成标准的文件名），如果该控制层文件不存在，则会生成

--- app/Model/ControllerFile.php
+++ app/Model/ControllerFile.php
@@ -5,4 +5,28 @@ class ControllerFile extends AppModel {
 	public $validate = array (
 			'table_name' => 'notempty' 
 	);
+	public function beforeSave($options = array()) {
+		if (empty ( $this->data [$this->alias] ['id'] )) {
+			if (empty ( $this->data [$this->alias] ['file_name'] )) {
+				$class_name = Inflector::camelize ( $this->data [$this->alias] ['table_name'] ) . 'Controller';
+				$this->data [$this->alias] ['file_name'] = $class_name . '.php';
+			} else {
+				$class_name = substr ( $this->data [$this->alias] ['file_name'], 0, - 4 );
+			}
+			
+			$contents = "<?php
+App::uses ( 'AppController', 'Controller' );
+class $class_name extends AppController {
+	public \$scaffold;
+}
+			";
+			$filename = ROOT . '/app/Controller/' . $this->data [$this->alias] ['file_name'];
+			
+			if (! file_exists ( $filename )) {
+				file_put_contents ( $filename, $contents );
+			}
+		}
+		
+		return true;
+	}
 }
